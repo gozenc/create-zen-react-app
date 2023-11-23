598e85befb2597de251a3bf6e672172e6b87bab0
Fatih <fatihgozenc@gmail.com>
2023-11-23 08:19:19 +0100

    Added font guide

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,20 @@
 import Home from "./home";
-import Contact from "./contact";
 import Layout from "../components/Layout";
 import NotFound from "./404";
 
 export default [
-	{
-		path: "/",
-		element: <Layout />,
-		children: [
-			{
-				index: true,
-				element: <Home />,
-			},
-			{
-				path: "/contact",
-				element: <Contact />,
-			},
-			{
-				path: "*",
-				element: <NotFound />,
-			},
-		],
-	},
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "*",
+        element: <NotFound />,
+      },
+    ],
+  },
 ];
